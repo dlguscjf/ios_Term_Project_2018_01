9c1a66d3c2ad1b40ff0416257414ee01bca1cb9e
D7703_06 <d7703_06@d7703-06ui-imac.local>
2018-06-25 17:00:47 +0900

    추가중 시작화면

---
+++ UiTableViewController Test 02/Controller/StartViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StartViewController.swift
+//  UiTableViewController Test 02
+//
+//  Created by D7703_06 on 2018. 6. 25..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         view.backgroundColor = UIColor.yellow
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
